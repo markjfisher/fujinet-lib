2a379013350015bbac5e5e53703d079db374a924
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2026-05-11 04:53:10 -0500

    Implement host prefix cmds (#29)
    * [adam][fn_fuji] implement fuji_get_host_prefix and fuji_set_host_prefix.
    
    * [fujinet-fuji] add FUJICMD_GET_HOST_PREFIX and FUJICMD_SET_HOST_PREFIX.
    
    * [apple2][fn_fuji] implement fuji_set_host_prefix.c
    
    * [adam][fn_fuji] change to use constant.
    
    * [apple2][fn_fuji] implement get_host_prefix, TODO: Implement in firmware.
    
    * [apple2][fn_fuji] implement missing includes.
    
    * [fn_fuji][apple2] i really am dumb.
    
    * [coco][fn_fuji] implement fuji_set_host_prefix()
    
    * [coco][fn_fuji] implement fuji_get_host_prefix()
    
    * [commodore][fn_fuji] implement fuji_set_host_prefix.
    
    * [commodore][fn_fuji] fuji_set_host_prefix, fix malloc error warning to be correct.
    
    * [commodore][fn_fuji] implement fuji_get_host_prefix()
    
    * [pmd85][fn_fuji] implement fuji_get_host_prefix and fuji_set_host_prefix
    
    * [adam][fn_fuji] implement fuji_get_host_prefix and fuji_set_host_prefix.
    
    * [pmd85] add missing header.
    
    * [commodore] remove malloc strategy.

--- commodore/src/fn_fuji/fuji_set_host_prefix.c
+++ commodore/src/fn_fuji/fuji_set_host_prefix.c
@@ -1,10 +1,21 @@
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 #include "fujinet-fuji.h"
 #include "fujinet-fuji-cbm.h"
 
+#define PL_BUF_SIZE 256
+
 bool fuji_set_host_prefix(uint8_t hs, char *prefix)
 {
-	// Not implemented in IEM fuji firmware
-	return false;
+  uint8_t pl[PL_BUF_SIZE];
+  uint16_t pl_len;
+
+  pl_len = strlen(prefix) + 3 + 1;
+
+  pl[0] = hs;
+  strcpy((char *) &pl[1], prefix);
+  pl[pl_len - 1] = '\0';
+
+  return open_close_data(FUJICMD_SET_HOST_PREFIX, true, pl_len, pl);
 }
